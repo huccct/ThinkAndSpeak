a84bfb6db0a42c111b0cde601ce59d895ac575fe
MuShan <mushanfu0@gmail.com>
2025-09-26 23:20:15 +0800

    feat(GlobalCorsConfig): 添加全局跨域配置新增全局 CORS 配置类，允许所有域名、请求头和请求方法，并支持携带 cookie。此配置解决了前后端分离架构下的跨域访问问题，确保前端应用能够正常调用后端 API 接口。

---
+++ backend/src/main/java/xyz/mushan/backend/common/config/GlobalCorsConfig.java
@@ -0,0 +1,29 @@
+package xyz.mushan.backend.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * @author mushan
+ */
+@Configuration
+public class GlobalCorsConfig {
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        // 允许所有域
+        config.addAllowedOriginPattern("*");
+        // 允许所有请求头
+        config.addAllowedHeader("*");
+        // 允许所有请求方法
+        config.addAllowedMethod("*");
+        // 允许携带cookie
+        config.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
