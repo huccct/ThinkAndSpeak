f1078d874ea5b9ee55bfd112ac030fe58667a05d
MuShan <mushanfu0@gmail.com>
2025-09-27 12:04:27 +0800

    fix: 日期序列化补充

--- backend/src/main/java/xyz/mushan/backend/modules/chat/dto/MessageDto.java
+++ backend/src/main/java/xyz/mushan/backend/modules/chat/dto/MessageDto.java
@@ -1,12 +1,16 @@
 package xyz.mushan.backend.modules.chat.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
-public record MessageDto(
+public record MessageDto (
         Long id,
         String sender,
         String content,
         String metadata,
-        LocalDateTime createdAt) implements Serializable {
+        @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+        LocalDateTime createdAt
+) implements Serializable {
 }
